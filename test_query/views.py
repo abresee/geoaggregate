b41f22f5163b74ff019a9e40a33a1dace654e6db
abresee <bresee.al@husky.neu.edu>
2013-06-23 19:34:45 -0400

    fixed csrf issues, cleaned up cruft

--- test_query/views.py
+++ test_query/views.py
@@ -1,20 +1,19 @@
-import simplejson
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.contrib.gis.geos import Point
-from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext
 from .models import WorldBorder
 
 def index(request):
     'Just a Map, yo'
-    return render_to_response('test_query/index.html')
+    return render_to_response('test_query/index.html',
+        context_instance=RequestContext(request))
 
-@csrf_exempt
 def query(request):
     post=request.POST
     point = Point(float(post['lng']),float(post['lat']))
     print(point)
     qs = WorldBorder.objects.filter(geom__contains=point)
     print(qs)
-    
+
     return HttpResponse("".join([i.name for i in qs])) 
