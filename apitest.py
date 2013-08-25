c76595045af3bfc9850dddd09abcf4953f3f66e6
jgravelin <jgravelin93@gmail.com>
2013-08-25 16:54:30 -0400

    corrected apitest.py

--- apitest.py
+++ apitest.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-from json import loads
+from json import loads,load,dump
 import os
 class ApiConnection:
     cachename = 'apicache'
@@ -7,20 +7,20 @@ class ApiConnection:
     def __init__(self):
         if os.path.exists(self.cachename):
             with open(self.cachename) as cache:
-                self.cache = json.load(cache)
+                self.cache = load(cache)
         else:
             self.cache = {}
 
     def save(self):
         with open(self.cachename,'w') as cache:
-            cache.write(json.dump(self.cache,indent=True))
+            dump(self.cache,cache,indent=True)
 
     def _call(self, call_type, id_str):
         eia_url="http://api.eia.gov/{t}?api_key=F4A37BD0D3E8D3ADC5E0D6671D2CB5FE&{t}_id={i}".format(t=call_type, i=id_str)
         return loads(urlopen(eia_url).read().decode('utf-8'))
 
     def _call_cache(self, call_type, id_str):
-        key = (call_type, id_str)
+        key = call_type + str(id_str)
         if key in self.cache:
             return self.cache[key]
         else:
